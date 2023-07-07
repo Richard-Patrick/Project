45245573fb10e7bd0d73b12fb2378197acb0dc36
Richard-Patrick <66868898+Richard-Patrick@users.noreply.github.com>
2023-07-08 00:04:19 +0100

    Add files via upload

---
+++ BFS.py
@@ -0,0 +1,22 @@
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            queue.extend(graph[node])
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+bfs(graph, 'C')
